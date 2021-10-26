83ed7f679ebb9c350106f50bdf533441d1a1daef
linux_china <libing.chen@gmail.com>
2021-10-26 15:27:20 -0700

    FoojayServiceTest

---
+++ src/test/java/org/apache/maven/plugins/toolchain/FoojayServiceTest.java
@@ -0,0 +1,22 @@
+package org.apache.maven.plugins.toolchain;
+
+
+import org.apache.maven.monitor.logging.DefaultLog;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
+import org.junit.Test;
+
+public class FoojayServiceTest {
+    FoojayService foojayService = new FoojayService(new DefaultLog(new ConsoleLogger()));
+
+    @Test
+    public void testParseDownloadUrl() throws Exception {
+        final String[] jdkLoadUrl = foojayService.parseFileNameAndDownloadUrl("17", "oracle-open-jdk");
+        System.out.println("filename: " + jdkLoadUrl[0]);
+        System.out.println("url: " + jdkLoadUrl[1]);
+    }
+
+    @Test
+    public void testDownloadAndExtract() throws Exception {
+        foojayService.downloadAndExtractJdk("15", "oracle_open_jdk");
+    }
+}
