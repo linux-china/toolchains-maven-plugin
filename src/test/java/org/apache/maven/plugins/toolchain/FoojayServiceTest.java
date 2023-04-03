c6e754304c691e55b5d1343c7d4a4f71d49caffb
Libing Chen <libing.chen@gmail.com>
2023-04-03 20:11:38 +0800

    test: add test testParseGraalVMDownloadUrl()

--- src/test/java/org/apache/maven/plugins/toolchain/FoojayServiceTest.java
+++ src/test/java/org/apache/maven/plugins/toolchain/FoojayServiceTest.java
@@ -6,11 +6,18 @@ import org.codehaus.plexus.logging.console.ConsoleLogger;
 import org.junit.Test;
 
 public class FoojayServiceTest {
-    FoojayService foojayService = new FoojayService(new DefaultLog(new ConsoleLogger()),null);
+    FoojayService foojayService = new FoojayService(new DefaultLog(new ConsoleLogger()), null);
 
     @Test
     public void testParseDownloadUrl() throws Exception {
         final String[] jdkLoadUrl = foojayService.parseFileNameAndDownloadUrl("17", "oracle-open-jdk");
+        System.out.println("filename: " + jdkLoadUrl[0]);
+        System.out.println("url: " + jdkLoadUrl[1]);
+    }
+
+    @Test
+    public void testParseGraalVMDownloadUrl() throws Exception {
+        final String[] jdkLoadUrl = foojayService.parseFileNameAndDownloadUrl("22.2", "graalvm_ce17");
         System.out.println("filename: " + jdkLoadUrl[0]);
         System.out.println("url: " + jdkLoadUrl[1]);
     }
